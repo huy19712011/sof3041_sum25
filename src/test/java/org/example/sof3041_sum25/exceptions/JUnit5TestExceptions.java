c71fec759a2cb2b54b10e2bb55ab5435ef913da7
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-05-28 23:29:18 +0700

    Exceptions tests

---
+++ src/test/java/org/example/sof3041_sum25/exceptions/JUnit5TestExceptions.java
@@ -0,0 +1,28 @@
+package org.example.sof3041_sum25.exceptions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JUnit5TestExceptions {
+
+    @Test
+    @DisplayName("Exceptions test")
+    void test() {
+        String str = null;
+        assertThrows(NullPointerException.class, () -> str.length());
+
+        Foo foo = new Foo();
+        Exception exception = assertThrows(Exception.class, () -> foo.foo());
+        assertEquals("Exception Message", exception.getMessage());
+    }
+
+}
+
+class Foo {
+    void foo() throws Exception {
+        throw new Exception("Exception Message");
+    }
+}
