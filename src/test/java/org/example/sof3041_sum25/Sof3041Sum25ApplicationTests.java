9a0ac2c8d20acd4f8a8369e7df4cecf2f2d9140d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-05-28 20:18:59 +0700

    Set up spring boot project

---
+++ src/test/java/org/example/sof3041_sum25/Sof3041Sum25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sof3041_sum25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3041Sum25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
